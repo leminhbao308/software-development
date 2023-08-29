4df137d3a670b0ef1a188b4f8790fa2ba5d6d1b8
leminhbao308 <besilent308@gmail.com>
2023-08-30 01:55:51 +0700

    Added Pie Chart to test

--- src/main/java/org/group06/view/screen/MainFrame.java
+++ src/main/java/org/group06/view/screen/MainFrame.java
@@ -1,7 +1,12 @@
 package org.group06.view.screen;
 
+import org.group06.utils.ColorConstant;
+import org.group06.utils.ImagePath;
+import org.group06.view.components.charts.PieChart;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.LinkedHashMap;
 
 public class MainFrame extends JFrame {
     public MainFrame() {
@@ -10,9 +15,17 @@ public class MainFrame extends JFrame {
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setUndecorated(true);
         setResizable(false);
-        setIconImage(new ImageIcon("src/main/resources/images/icon/icon.png").getImage());
-        JLabel lbl = new JLabel();
-        lbl.setIcon(new ImageIcon("src/main/resources/images/icon/icon.png"));
-        this.add(lbl, BorderLayout.CENTER);
+        setIconImage(new ImageIcon(ImagePath.ICON_LOGO).getImage());
+
+        LinkedHashMap<String, Double> data = new LinkedHashMap<>();
+        data.put("Category 01", 400.0);
+        data.put("Category 02", 1204.0);
+        data.put("Category 03", 4909.0);
+        data.put("Category 04", 789.0);
+        data.put("Category 05", 400.0);
+
+        PieChart pieChart = new PieChart(data);
+        add(pieChart, BorderLayout.CENTER);
+        add(pieChart.createLegendPanel(data, ColorConstant.generateColorPalette(data.size())), BorderLayout.EAST);
     }
 }
