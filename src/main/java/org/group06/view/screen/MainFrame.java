1312d6b2ca70aa0f9ff5532f8e81f5a4a77ddcfd
Le Hoang Nam <lehoangnam.31123@gmail.com>
2023-08-30 16:28:10 +0700

    demo pie chart

--- src/main/java/org/group06/view/screen/MainFrame.java
+++ src/main/java/org/group06/view/screen/MainFrame.java
@@ -1,11 +1,14 @@
 package org.group06.view.screen;
 
+import org.group06.model.entity.Customer;
+import org.group06.model.manager.CustomerManager;
 import org.group06.utils.ColorConstant;
 import org.group06.utils.ImagePath;
 import org.group06.view.components.charts.PieChart;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
 public class MainFrame extends JFrame {
@@ -18,11 +21,17 @@ public class MainFrame extends JFrame {
         setIconImage(new ImageIcon(ImagePath.ICON_LOGO).getImage());
 
         LinkedHashMap<String, Double> data = new LinkedHashMap<>();
-        data.put("Category 01", 400.0);
-        data.put("Category 02", 1204.0);
-        data.put("Category 03", 4909.0);
-        data.put("Category 04", 789.0);
-        data.put("Category 05", 400.0);
+        ArrayList<Customer> c1 = new ArrayList<>();
+        c1.add(new Customer("NV01", "Le Minh Bao", "hcm", "012345678", 100.0));
+        c1.add(new Customer("NV02", "Le Minh Beo", "hcm", "012345678", 103.0));
+        c1.add(new Customer("NV03", "Le Minh Lon", "hcm", "012345678", 1023.0));
+        c1.add(new Customer("NV04", "Le Minh Nho", "hcm", "012345678", 1450.0));
+
+        CustomerManager cm = new CustomerManager(c1);
+
+        for ( Customer c : cm.getCustomers()) {
+            data.put(c.getTen(), c.getTongThanhToan());
+        }
 
         PieChart pieChart = new PieChart(data);
         add(pieChart, BorderLayout.CENTER);
