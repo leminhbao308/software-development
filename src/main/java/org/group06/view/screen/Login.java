19e9fb1998e0d54d3c9e99389983c38ac1ccb3dd
Le Hoang Nam <lehoangnam.31123@gmail.com>
2023-10-11 12:00:06 +0700

    Recode login, add new Color. Create form demo. Modify Txt, btn, pwd

--- src/main/java/org/group06/view/screen/Login.java
+++ src/main/java/org/group06/view/screen/Login.java
@@ -14,7 +14,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Login extends JFrame implements ActionListener {
-    private RoundButton btnForgotPwd;
+
+//    private RoundButton btnForgotPwd;
     private RoundButton btnLogin;
     private javax.swing.JLabel lblSubTitle;
     private javax.swing.JLabel lblTitle;
@@ -34,58 +35,50 @@ public class Login extends JFrame implements ActionListener {
         lblSubTitle = new javax.swing.JLabel();
         txtUsername = new TextField();
         pwdPassword = new PasswordField();
-        btnForgotPwd = new RoundButton("Forgot Password", ColorConstant.BUTTON_SECONDARY_NORMAL, ColorConstant.BUTTON_SECONDARY_HOVER, ColorConstant.BUTTON_SECONDARY_CLICK);
-        btnLogin = new RoundButton("LOGIN", ColorConstant.BUTTON_NORMAL, ColorConstant.BUTTON_HOVER, ColorConstant.BUTTON_CLICK);
-        pnImage = new ImagePanel(ImagePath.THUMBNAIL_LOGIN, 653, 600);
+        btnLogin = new RoundButton("LOGIN", ColorConstant.BUTTON_LOGIN_NORMAL, ColorConstant.BUTTON_LOGIN_HOVER, ColorConstant.BUTTON_LOGIN_CLICK);
+        pnImage = new ImagePanel(ImagePath.THUMBNAIL_LOGIN, 820, 620);
 
+        setUndecorated(true);
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Login - Group 06");
+        setTitle("Đăng Nhập - Nhóm 06");
         setIconImage(new ImageIcon(ImagePath.ICON_LOGO).getImage());
         setResizable(false);
-        setMaximumSize(new java.awt.Dimension(1000, 550));
-        setMinimumSize(new java.awt.Dimension(1000, 550));
-        setPreferredSize(new java.awt.Dimension(1000, 550));
-        setBackground(ColorConstant.TEXT_NORMAL);
+        setSize(new java.awt.Dimension(1400, 620));
+        setBackground(ColorConstant.BACKGROUND_NORMAL);
 
-        pnLogin.setBackground(ColorConstant.BACKGROUND_GRAY);
         pnLogin.setMaximumSize(new java.awt.Dimension(447, 600));
         pnLogin.setMinimumSize(new java.awt.Dimension(447, 600));
         pnLogin.setPreferredSize(new java.awt.Dimension(447, 600));
-        pnLogin.setBackground(ColorConstant.BACKGROUND_BLACK);
+        pnLogin.setBackground(ColorConstant.BACKGROUND_WHITE);
 
-        lblTitle.setFont(new java.awt.Font("Segoe UI Black", 1, 30));
+        lblTitle.setFont(new java.awt.Font("Segoe UI", 1, 30));
         lblTitle.setForeground(ColorConstant.TEXT_HEADER_FOOTER);
-        lblTitle.setText("Login Your Account");
+        lblTitle.setText("Đăng Nhập Tài Khoản");
         lblTitle.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
         lblTitle.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
 
-        lblSubTitle.setText("Please login to your account");
+        lblSubTitle.setText("Vui lòng nhập tài khoản của bạn");
         lblSubTitle.setFont(new java.awt.Font("Segoe UI", Font.PLAIN, 16));
         lblSubTitle.setForeground(ColorConstant.TEXT_HEADER_FOOTER);
         lblSubTitle.setVerticalAlignment(javax.swing.SwingConstants.TOP);
         lblSubTitle.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
 
         txtUsername.setFont(new java.awt.Font("Segoe UI", 0, 18));
-        txtUsername.setHint("Username or Email");
-        txtUsername.setToolTipText("Enter Username");
+        txtUsername.setHint("Mã nhân viên");
+        txtUsername.setToolTipText("Nhập vào mã nhân viên");
         txtUsername.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
 
         pwdPassword.setFont(new java.awt.Font("Segoe UI", 0, 18));
-        pwdPassword.setHint("Password");
-        pwdPassword.setToolTipText("Enter Password");
+        pwdPassword.setHint("Mật khẩu");
+        pwdPassword.setToolTipText("Nhập vào mật khẩu");
         pwdPassword.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
 
-        btnForgotPwd.setToolTipText("Click if you forgot your password");
-        btnForgotPwd.setBorderPainted(false);
-        btnForgotPwd.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        btnForgotPwd.addActionListener(this);
-
-
-        btnLogin.setFont(new java.awt.Font("Segoe UI", 1, 14));
+        btnLogin.setFont(new java.awt.Font("Segoe UI", 1, 18));
         btnLogin.setForeground(ColorConstant.TEXT_BUTTON);
-        btnLogin.setText("LOGIN");
-        btnLogin.setToolTipText("Click to login to your account");
+        btnLogin.setText("Đăng Nhập");
+        btnLogin.setToolTipText("Click để đăng nhập");
         btnLogin.addActionListener(new EventLogin(this));
+        btnLogin.setCursor(new java.awt.Cursor(Cursor.HAND_CURSOR));
 
         javax.swing.GroupLayout pnLoginLayout = new javax.swing.GroupLayout(pnLogin);
         pnLogin.setLayout(pnLoginLayout);
@@ -93,15 +86,13 @@ public class Login extends JFrame implements ActionListener {
                 pnLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(pnLoginLayout.createSequentialGroup()
                                 .addContainerGap(78, Short.MAX_VALUE)
-                                .addGroup(pnLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addGroup(pnLoginLayout.createParallelGroup(GroupLayout.Alignment.CENTER, false)
                                         .addComponent(txtUsername, javax.swing.GroupLayout.Alignment.TRAILING)
                                         .addComponent(lblTitle, javax.swing.GroupLayout.DEFAULT_SIZE, 318, Short.MAX_VALUE)
                                         .addComponent(lblSubTitle, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                         .addComponent(pwdPassword, javax.swing.GroupLayout.Alignment.TRAILING)
-                                        .addGroup(pnLoginLayout.createSequentialGroup()
-                                                .addComponent(btnForgotPwd)
-                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                                .addComponent(btnLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 185, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                        .addGroup(pnLoginLayout.createSequentialGroup().addComponent(btnLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 185, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                )
                                 .addGap(51, 51, 51))
         );
         pnLoginLayout.setVerticalGroup(
@@ -116,10 +107,11 @@ public class Login extends JFrame implements ActionListener {
                                 .addGap(18, 18, 18)
                                 .addComponent(pwdPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGap(70, 70, 70)
                                 .addGroup(pnLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
-                                        .addComponent(btnForgotPwd)
-                                        .addComponent(btnLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addContainerGap(119, Short.MAX_VALUE))
+                                        .addComponent(btnLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addContainerGap(90, Short.MAX_VALUE)
+                                )
         );
 
         javax.swing.GroupLayout pnImageLayout = new javax.swing.GroupLayout(pnImage);
@@ -153,9 +145,6 @@ public class Login extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        Object o = e.getSource();
-        if (o == btnForgotPwd) {
-            JOptionPane.showMessageDialog(null, "Username: Admin\nPassword: Admin", "Forgot Password", JOptionPane.INFORMATION_MESSAGE);
-        }
+
     }
 }
