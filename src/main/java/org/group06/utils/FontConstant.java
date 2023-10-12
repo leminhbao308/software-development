062c836963d7d5367b1c6eae8f01ec6bd11fa316
Lê Minh Bảo <besilent308@gmail.com>
2023-10-12 22:49:45 +0700

    Add some new constants

---
+++ src/main/java/org/group06/utils/FontConstant.java
@@ -0,0 +1,16 @@
+package org.group06.utils;
+
+import java.awt.*;
+
+public class FontConstant {
+
+    public static final Font FONT_HEADER = new Font("Segoe UI Black", Font.BOLD, 30);
+    public static final Font FONT_SUB_HEADER = new Font("Segoe UI", Font.BOLD, 16);
+    public static final Font FONT_LABEL = new Font("Segoe UI", Font.PLAIN, 18);
+    public static final Font FONT_TEXT = new Font("Segoe UI", Font.PLAIN, 18);
+    public static final Font FONT_BUTTON = new Font("Segoe UI", Font.BOLD, 14);
+
+    private FontConstant() {
+        // Để ngăn việc tạo đối tượng instance của lớp này
+    }
+}
--- src/main/java/org/group06/utils/ImagePath.java
+++ src/main/java/org/group06/utils/ImagePath.java
@@ -1,16 +1,33 @@
 package org.group06.utils;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 import java.net.URL;
 
 public class ImagePath {
     // Icon path
     public static final URL ICON_LOGO = ImagePath.class.getResource("/images/icon/icon.png");
-    public static final URL ICON_CANCEL = ImagePath.class.getResource("/images/icon/cancel.png");
+    public static final URL ICON_USER = ImagePath.class.getResource("/images/icon/user.png");
 
     // Thumbnail path
     public static final URL THUMBNAIL_LOGIN = ImagePath.class.getResource("/images/thumbnail/login.jpg");
+    public static final URL THUMBNAIL_LOGO = ImagePath.class.getResource("/images/logo/logo.png");
 
     private ImagePath() {
         // Để ngăn việc tạo đối tượng instance của lớp này
+    }
+
+    public static Image loadImage(URL iconLogo) {
+        try {
+            return ImageIO.read(iconLogo);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Icon loadIcon(URL iconLogo) {
+        return new ImageIcon(loadImage(iconLogo));
     }
 }
