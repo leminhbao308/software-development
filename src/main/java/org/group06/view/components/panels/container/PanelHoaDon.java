90a7c9428fc14e4fb97a94e2d86afd7a167164ee
Bao44 <tqbao44@gmail.com>
2023-10-27 20:38:05 +0700

    fix PanelHoaDon,PanelPhieuTam

--- src/main/java/org/group06/view/components/panels/container/PanelHoaDon.java
+++ src/main/java/org/group06/view/components/panels/container/PanelHoaDon.java
@@ -30,46 +30,76 @@ public class PanelHoaDon extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        lblTitleHoaDon = new javax.swing.JLabel();
-        btnTimMaHoaDon = new org.group06.view.components.buttons.StaticRoundButton("", ImagePath.ICON_SEARCH, ColorConstant.BUTTON_LOGIN_NORMAL, ColorConstant.BUTTON_LOGIN_HOVER, ColorConstant.BUTTON_LOGIN_CLICK);
-        txtTimMaHoaDon = new org.group06.view.components.textFields.TextField("Mã hoá đơn cần tìm", ColorConstant.BLACK);
-        scrHoaDon = new javax.swing.JScrollPane();
+        lblTitleDSHoaDon = new javax.swing.JLabel();
+        pnlTimHD = new javax.swing.JPanel();
+        lblTimTheoTenKH = new javax.swing.JLabel();
+        lblTimTheoNgay = new javax.swing.JLabel();
+        txtTimTheoTenKH = new javax.swing.JTextField();
+        txtTimTheoNgay = new javax.swing.JTextField();
+        srcHoaDon = new javax.swing.JScrollPane();
         tblHoaDon = new javax.swing.JTable();
 
-        setBackground(ColorConstant.BACKGROUND_CONTAINER);
+        lblTitleDSHoaDon.setFont(new java.awt.Font("Segoe UI", 1, 28)); // NOI18N
+        lblTitleDSHoaDon.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblTitleDSHoaDon.setText("Danh sách hóa đơn");
 
-        lblTitleHoaDon.setFont(org.group06.utils.FontConstant.FONT_HEADER);
-        lblTitleHoaDon.setForeground(org.group06.utils.ColorConstant.WHITE);
-        lblTitleHoaDon.setText("Thông Tin Hoá Đơn");
+        pnlTimHD.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)), "Tìm Kiếm Hóa Đơn", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Segoe UI", 1, 14))); // NOI18N
 
-        btnTimMaHoaDon.setToolTipText("Click để tìm kiếm khách hàng");
-        btnTimMaHoaDon.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        btnTimMaHoaDon.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        lblTimTheoTenKH.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lblTimTheoTenKH.setText("Tìm theo tên khách hàng");
 
-        txtTimMaHoaDon.setFont(FontConstant.FONT_TEXT);
-        txtTimMaHoaDon.setToolTipText("Nhập mã hoá đơn cần tìm");
-        txtTimMaHoaDon.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtTimMaHoaDonActionPerformed(evt);
-            }
-        });
+        lblTimTheoNgay.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lblTimTheoNgay.setText("Tìm theo ngày lập hóa đơn");
+
+        txtTimTheoTenKH.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        txtTimTheoTenKH.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        txtTimTheoTenKH.setPreferredSize(new java.awt.Dimension(64, 29));
+
+        txtTimTheoNgay.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        txtTimTheoNgay.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        txtTimTheoNgay.setPreferredSize(new java.awt.Dimension(64, 29));
+
+        javax.swing.GroupLayout pnlTimHDLayout = new javax.swing.GroupLayout(pnlTimHD);
+        pnlTimHD.setLayout(pnlTimHDLayout);
+        pnlTimHDLayout.setHorizontalGroup(
+            pnlTimHDLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlTimHDLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lblTimTheoTenKH)
+                .addGap(18, 18, 18)
+                .addComponent(txtTimTheoTenKH, javax.swing.GroupLayout.PREFERRED_SIZE, 186, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(lblTimTheoNgay)
+                .addGap(18, 18, 18)
+                .addComponent(txtTimTheoNgay, javax.swing.GroupLayout.PREFERRED_SIZE, 193, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(12, 12, 12))
+        );
+        pnlTimHDLayout.setVerticalGroup(
+            pnlTimHDLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlTimHDLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(pnlTimHDLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
+                    .addComponent(lblTimTheoNgay, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblTimTheoTenKH)
+                    .addComponent(txtTimTheoTenKH, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtTimTheoNgay, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(12, Short.MAX_VALUE))
+        );
+
+        srcHoaDon.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
 
         tblHoaDon.setAutoCreateRowSorter(true);
-        tblHoaDon.setBackground(org.group06.utils.ColorConstant.BACKGROUND_NORMAL);
-        tblHoaDon.setFont(org.group06.utils.FontConstant.FONT_TEXT);
         tblHoaDon.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null}
+                {"HD001", "11/12/2021", "Trần văn cường", "Bùi thị hoa", "120000"},
+                {"HD002", "28/2/2023", "Nguyễn Thị Ly", "Trương Văn Toàn", "96000"}
             },
             new String [] {
-                "Mã hoá đơn", "Ngày tạo hoá đơn", "Tên khách hàng", "Nhân viên lập hoá đơn", "Tổng thành tiền"
+                "Mã hóa đơn", "Ngày lập hóa đơn", "Tên khách hàng", "Tên nhân viên lập hóa đơn", "Tổng thành tiền"
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.String.class, java.lang.Object.class, java.lang.String.class, java.lang.String.class, java.lang.Double.class
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
             };
 
             public Class getColumnClass(int columnIndex) {
@@ -77,76 +107,50 @@ public class PanelHoaDon extends javax.swing.JPanel {
             }
         });
         tblHoaDon.setAutoResizeMode(javax.swing.JTable.AUTO_RESIZE_ALL_COLUMNS);
-        tblHoaDon.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         tblHoaDon.setFillsViewportHeight(true);
-        tblHoaDon.setGridColor(org.group06.utils.ColorConstant.BLACK);
-        tblHoaDon.setRowHeight(50);
-        tblHoaDon.setSelectionBackground(org.group06.utils.ColorConstant.BACKGROUND_SIDEBAR);
-        tblHoaDon.setSelectionForeground(org.group06.utils.ColorConstant.WHITE);
+        tblHoaDon.setGridColor(new java.awt.Color(0, 0, 0));
         tblHoaDon.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
-        tblHoaDon.setShowGrid(false);
-        tblHoaDon.setShowHorizontalLines(true);
-        tblHoaDon.getTableHeader().setResizingAllowed(false);
-        tblHoaDon.getTableHeader().setReorderingAllowed(false);
-        tblHoaDon.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                tblHoaDonMouseClicked(evt);
-            }
-        });
-        scrHoaDon.setViewportView(tblHoaDon);
+        tblHoaDon.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        tblHoaDon.setShowGrid(true);
+        srcHoaDon.setViewportView(tblHoaDon);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(lblTitleHoaDon, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(txtTimMaHoaDon, javax.swing.GroupLayout.PREFERRED_SIZE, 527, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btnTimMaHoaDon, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(scrHoaDon, javax.swing.GroupLayout.DEFAULT_SIZE, 818, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(srcHoaDon, javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(pnlTimHD, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(lblTitleDSHoaDon, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(btnTimMaHoaDon, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(txtTimMaHoaDon)
-                    .addComponent(lblTitleHoaDon, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(lblTitleDSHoaDon, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(scrHoaDon, javax.swing.GroupLayout.DEFAULT_SIZE, 510, Short.MAX_VALUE)
+                .addComponent(pnlTimHD, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(srcHoaDon, javax.swing.GroupLayout.DEFAULT_SIZE, 422, Short.MAX_VALUE)
                 .addContainerGap())
         );
+
+        pnlTimHD.getAccessibleContext().setAccessibleName("Tìm Kiếm hóa đơn");
     }// </editor-fold>//GEN-END:initComponents
-
-    private void txtTimMaHoaDonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtTimMaHoaDonActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txtTimMaHoaDonActionPerformed
-
-    private void tblHoaDonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblHoaDonMouseClicked
-//        if(evt.getClickCount() == 2) {
-//            JFrame jf = new JFrame();
-//            jf.add(new PanelTTKhachHang());
-//            jf.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-//            jf.setLocationRelativeTo(null);
-//            jf.setResizable(false);
-////            jf.pack();
-//            jf.setVisible(true);
-//        }
-    }//GEN-LAST:event_tblHoaDonMouseClicked
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnTimMaHoaDon;
-    private javax.swing.JLabel lblTitleHoaDon;
-    private javax.swing.JScrollPane scrHoaDon;
+    private javax.swing.JLabel lblTimTheoNgay;
+    private javax.swing.JLabel lblTimTheoTenKH;
+    private javax.swing.JLabel lblTitleDSHoaDon;
+    private javax.swing.JPanel pnlTimHD;
+    private javax.swing.JScrollPane srcHoaDon;
     private javax.swing.JTable tblHoaDon;
-    private javax.swing.JTextField txtTimMaHoaDon;
+    private javax.swing.JTextField txtTimTheoNgay;
+    private javax.swing.JTextField txtTimTheoTenKH;
     // End of variables declaration//GEN-END:variables
 }
