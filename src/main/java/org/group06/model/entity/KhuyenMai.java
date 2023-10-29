a2fd25c5c00afcba655a2dc5001ab956064aae25
Lê Minh Bảo <besilent308@gmail.com>
2023-10-30 06:36:42 +0700

    Modify default constructor KhachHang and KhuyenMai

--- src/main/java/org/group06/model/entity/KhuyenMai.java
+++ src/main/java/org/group06/model/entity/KhuyenMai.java
@@ -14,8 +14,8 @@ public class KhuyenMai {
     private int soLuotSuDung;
 
     public KhuyenMai() {
-        this.maKhuyenMai = "Chưa xác định";
-        this.tenCTKM = "Chưa xác định";
+        this.maKhuyenMai = "Không";
+        this.tenCTKM = "Không";
         this.mucGiamGia = 0;
         this.ngayBatDau = new Date(System.currentTimeMillis());
         this.ngayKetThuc = this.ngayBatDau;
