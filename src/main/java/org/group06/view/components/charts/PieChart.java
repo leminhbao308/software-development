037038e821ce49a39de5a3478b6443d193a9fcf1
Bao44 <tqbao44@gmail.com>
2023-11-14 15:07:07 +0700

    create biểu đồ ThongKeKhachHang

--- src/main/java/org/group06/view/components/charts/PieChart.java
+++ src/main/java/org/group06/view/components/charts/PieChart.java
@@ -3,6 +3,8 @@ package org.group06.view.components.charts;
 import org.group06.view.components.charts.data.PieChartData;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
+import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
+import org.jfree.chart.plot.PiePlot;
 import org.jfree.chart.title.TextTitle;
 
 import java.awt.*;
@@ -11,7 +13,8 @@ public class PieChart extends ChartPanel {
 
     public PieChart(String title, PieChartData data) {
         super(ChartFactory.createPieChart(title, data.getDataSet(), true, true, false));
-//        chart.getPlot().set
+        setPercent();
+        this.setScrollable(true);
     }
 
     public PieChart(String title, String subTitle, PieChartData data) {
@@ -20,6 +23,13 @@ public class PieChart extends ChartPanel {
         // Add subtitle
         TextTitle source = new TextTitle(subTitle, new Font("Courier New", Font.PLAIN, 12));
         super.getChart().addSubtitle(source);
+        setPercent();
+        this.setScrollable(true);
+    }
+
+    private void setPercent() {
+        PiePlot plot = (PiePlot) super.getChart().getPlot();
+        plot.setLabelGenerator(new StandardPieSectionLabelGenerator("{0} = {2}"));
     }
 
     public void setScrollable(boolean scrollable) {
